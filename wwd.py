d322fe8f020e3ced4bf0557db62a9535aab1b827
Anthony Gabriele <ripleyintheair@users.noreply.github.com>
2015-11-23 19:14:40 -0500

    Update wwd.py

--- wwd.py
+++ wwd.py
@@ -7,13 +7,23 @@ client=dropbox.client.DropboxClient(TOKEN_FILE_HERE)
 oldfile=client.metadata('/WWD')['contents'][0]['path']
 delo=client.file_delete(oldfile)
 # pull the latest PDF from WWD.com
-d=date.today()
-y=d.strftime('%Y')
-m=d.strftime('%m')
-da=d.strftime('%d')
-z='https://pmcwwd.files.wordpress.com/'+y+'/'+m+'/wwd'+m+da+'web.pdf'
+opener = urllib2.build_opener(
+    urllib2.HTTPRedirectHandler(),
+    urllib2.HTTPHandler(debuglevel=0),
+    urllib2.HTTPSHandler(debuglevel=0)
+)
+opener.addheaders = [
+    ('User-agent', ('Mozilla/4.0 (compatible; MSIE 6.0; '
+                   'Windows NT 5.2; .NET CLR 1.1.4322)'))
+]
+html = opener.open("http://www.wwd.com")
+soup = bs4.BeautifulSoup(html)
+link = soup.find(class_="pub-pdf")['href']
+html = opener.open(link)
+soup = bs4.BeautifulSoup(html)
+z = soup.find(class_="pdf")['href']
 response=urllib2.urlopen(z)
 # store it in Dropbox
 upload=client.put_file('/WWD/'+z.split('/')[-1],response.read())
 response.close()
-# IfTTT will then push to Kindle; cron this to run every Weds morning
+# IfTTT will then push to Kindle; cron this to run every Wednesday morning
